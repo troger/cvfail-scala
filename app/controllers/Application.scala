789ab9072f8b4e9c9a842aa7088aab445480308d
Thomas Roger <throger@gmail.com>
2010-03-21 21:41:50 +0100

    Add home page listing fails

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,10 +1,17 @@
 package controllers
 
-import play._
 import play.mvc._
+import models.Fail
 
 object Application extends Controller {
+
+  def index = {
+    val fails = Fail.find("order by postedAt desc").fetch
+    render(fails)
+  }
+
+  def postFail() = {
     
-    def index = render()
-    
+  }
+
 }
