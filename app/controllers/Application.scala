4de93db91776617c729c1a960ab0533896b1fa9f
Thomas Roger <throger@gmail.com>
2010-03-22 00:31:07 +0100

    Add OpenID authentication to create a fail

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,8 +3,13 @@ package controllers
 import play.mvc._
 import models.Fail
 import play.data.validation.{Validation, Required}
+import play.libs.OpenID
+import play.libs.OpenID.UserInfo
 
 object Application extends Controller {
+
+  private val discoveryUrls = Map("google" -> "https://www.google.com/accounts/o8/id",
+    "yahoo" -> "https://me.yahoo.com/")
 
   def index = {
     val fails = Fail.find("order by postedAt desc").fetch
@@ -12,7 +17,33 @@ object Application extends Controller {
   }
 
   def create = {
+    if(!session.contains("user")) {
+      login
+    }
     render()
+  }
+
+  def login = {
+    render()
+  }
+
+  def logout = {
+    session.remove("user")
+    index
+  }
+
+  def authenticate(provider: String): Unit = {
+    if (OpenID.isAuthenticationResponse) {
+      val verifiedUser = OpenID.getVerifiedID
+        if(verifiedUser == null) {
+            flash.put("error", "Authentication has failed")
+            login
+        }
+        session.put("user", verifiedUser.id)
+        create
+    } else {
+      OpenID.id(discoveryUrls(provider)).verify
+    }
   }
 
   def postFail(@Required(message = "A message is required") message: String,
